8c18840c1c646892f8105c1c81e20bba74e264a3
ehullander <eric.hullander@gmail.com>
2021-03-18 03:15:53 +0000

    plexus test

--- NiFi/src/.ipynb_checkpoints/twitterplot2-checkpoint.py
+++ NiFi/src/.ipynb_checkpoints/twitterplot2-checkpoint.py
@@ -6,15 +6,19 @@ import psycopg2
 import plotly.express as px
 import pandas as pd
 import sys
+import pathlib
+
 def main():
-    # df = pd.read_csv("/home/acc/twitter.csv")
+    # output path parallel to src code in directory tree
+    path = pathlib.Path().parent.absolute().parent.joinpath('output')
+    output = str(path)
+    # read flowfile from stdin
     df = pd.read_csv(sys.stdin)
-    #df.to_csv("/home/acc/output/last.csv")
+    # transform
     df = df.drop(0)
     df.columns = ["full_name", "time", "text", "lat", "lon", "sentiment"]
     df['sentiment'] = (df['sentiment'] - 5)/10
     df = df.dropna()
-    # transform
     df.lat = df.lat.astype('float')
     df.lon = df.lon.astype('float')
     # aggregate
@@ -23,13 +27,11 @@ def main():
     DF = df.groupby('full_name').mean()
     DF['n'] = df.groupby('full_name').count()['sentiment'].values
     DF = DF.sort_values('n', ascending = False).reset_index()
-    # filter out small towns
-    # DF = DF[DF['n'] > thresh]
     # plot aggregates
     fig = px.scatter_mapbox(DF, lat="lat", lon="lon",  hover_data=["full_name"], mapbox_style="carto-positron", color="sentiment", size = "n", zoom = 3.5, opacity = .5)
-    pio.write_json(fig, '/home/acc/NiFi/output/tweet1.plotly')
+    pio.write_json(fig, output + '/tweet1.plotly')
     # plot individual data
     fig = px.scatter_mapbox(df, lat="lat", lon="lon",  hover_data=["text", "full_name", "sentiment"], mapbox_style="carto-positron", color="sentiment", zoom = 3.5, opacity = .5)
-    pio.write_json(fig, '/home/acc/NiFi/output/tweet2.plotly')
+    pio.write_json(fig, output + '/tweet2.plotly')
 if __name__ == "__main__":
     main()
